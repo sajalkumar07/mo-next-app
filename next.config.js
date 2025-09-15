67016d8b4d5f23536f5a51420e45cb78820a29c0
sajalkumar07 <sajalkumarr030@gmail.com>
2025-09-15 19:42:51 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-  // This will skip static generation and use client-side rendering
-  exportPathMap: function () {
+module.exports = {
+  exportPathMap: async function () {
     return {
       "/": { page: "/" },
     };
   },
 };
-
-module.exports = nextConfig;
