5cdfd89c631a99f8b4af2b9cfe6d202726949dd4
sajalkumar07 <sajalkumarr030@gmail.com>
2025-09-15 20:43:02 +0530

    update env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,18 @@
-// module.exports = {
-//   apps: [
-//     {
-//       name: "new-mo-next-app",
-//       script: "npm",
-//       args: "start",
-//       cwd: "/root/new-mo-next-app",
-//       env: {
-//         NODE_ENV: "production",
-//         PORT: 4000,
-//       },
-//     },
-//   ],
-// };
+require("dotenv").config({ path: ".env.qa" });
+
+module.exports = {
+  apps: [
+    {
+      name: "motor-ocatain",
+      cwd: "/root/new-mo-next-app",
+      script: "npm",
+      args: "run start",
+      env: {
+        NODE_ENV: "production",
+        HOST: "0.0.0.0",
+        PORT: "4000",
+        NEXT_PUBLIC_API: process.env.NEXT_PUBLIC_API,
+      },
+    },
+  ],
+};
